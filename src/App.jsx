861fe081ddde3b0a5747d49d7b241b08fcd0df16
Camilo Torres <racknarow1@gmail.com>
2022-07-12 22:35:52 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Appletras from './components/Appletras'
+
+function App() {
+
+
+  return (
+    <Appletras >
+
+    </Appletras>
+  )
+}
+
+export default App
---
+++ src/components/Appletras.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Formulario from '../components/Formulario'
+
+const Appletras = () => {
+  return (
+    <>
+      <header>Búsqueda de Letras de Canciones</header>
+      <Formulario />
+
+      <main> 
+
+
+      </main>
+    </>
+  )
+}
+
+export default Appletras
